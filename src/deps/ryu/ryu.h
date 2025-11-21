0d235eee40996f821a1c46f8bf20fb93598f2087
Bryson Steck <bryson.steck@sdl.usu.edu>
2025-11-21 09:29:12 -0700

    set up ryu to build dynamically

--- src/deps/ryu/ryu.h
+++ src/deps/ryu/ryu.h
@@ -17,22 +17,37 @@
 #ifndef RYU_H
 #define RYU_H
 
+#if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || \
+    defined(__BCPLUSPLUS__) || defined(__MWERKS__)
+
+#if defined(GEOS_DLL_EXPORT)
+#define GEOS_DLL __declspec(dllexport)
+#elif defined(GEOS_DLL_IMPORT)
+#define GEOS_DLL extern __declspec(dllimport)
+#else
+#define GEOS_DLL
+#endif
+#else
+#define GEOS_DLL
+#endif
+
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <inttypes.h>
 
-/* Print the shortest representation of a double using fixed notation
- * Only works for numbers smaller than 1e+17 (absolute value)
- * Precision limits the amount of digits of the decimal part
- */
-int geos_d2sfixed_buffered_n(double f, uint32_t precision, char* result);
+  /* Print the shortest representation of a double using fixed notation
+   * Only works for numbers smaller than 1e+17 (absolute value)
+   * Precision limits the amount of digits of the decimal part
+   */
+  GEOS_DLL int geos_d2sfixed_buffered_n(double f, uint32_t precision, char *result);
 
-/* Print the shortest representation of a double using scientific notation
- * Precision limits the amount of digits of the decimal part
- */
-int geos_d2sexp_buffered_n(double f, uint32_t precision, char* result);
+  /* Print the shortest representation of a double using scientific notation
+   * Precision limits the amount of digits of the decimal part
+   */
+  GEOS_DLL int geos_d2sexp_buffered_n(double f, uint32_t precision, char *result);
 
 #ifdef __cplusplus
 }
